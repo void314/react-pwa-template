10dfeba39b8615c223b2f289f806a837710da950
Artem <71975200+void314@users.noreply.github.com>
2025-03-19 22:08:30 +0500

    Updates icon and config for PWA support
    Replaces the favicon.svg with icon.svg to align with PWA requirements.
    Updates the Vite config to correctly handle the new icon path
    and configures PWA options, including auto-update and workbox settings.
    Removes tsconfig.app.json file and updates tsconfig.json to prevent build errors.
    Adds a "Hello World" paragraph in App.tsx to test the changes.

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ function App() {
     return (
         <div className="flex min-h-svh flex-col items-center justify-center">
             <Button>Click me</Button>
+            <p>Hello World</p>
             <div className="flex items-center gap-2">
                 <ModeToggle />
             </div>
--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,33 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import { env } from 'process';
 import { defineConfig } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
 
-let faviconURL = '/favicon.svg';
+let faviconURL = '/icon.svg';
 
-// https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
         react(),
         tailwindcss(),
         VitePWA({
             includeAssets: [faviconURL],
+            registerType: 'autoUpdate',
+            mode: 'development',
+            minify: false,
+            strategies: 'generateSW',
+            filename: 'sw.js',
+            selfDestroying: true,
+            workbox: {
+                globPatterns: ['**/*.{js,css,html}'],
+                globIgnores: ['**/node_modules/**', '**/dist/**', '**/build/**', '**/public/**', '**/src/**'],
+                offlineGoogleAnalytics: false,
+            },
+            devOptions: {
+                enabled: true,
+                type: 'module',
+            },
             manifest: {
                 theme_color: '#ffffff',
                 icons: [
