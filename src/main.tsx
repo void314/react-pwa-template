178605705c1ea48613b5427681cf6e1f73562ff4
Artem <71975200+void314@users.noreply.github.com>
2025-03-19 20:40:18 +0500

    Enables client-side routing
    Configures `BrowserRouter` with a root route to enable
    client-side navigation using React Router.

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from 'react-router';
 
 import App from './App';
 import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<App />} />
+        </Routes>
+    </BrowserRouter>
 );
 
